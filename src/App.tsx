611ff39a651009060fddb512122accfbcd500a15
Greg French <gregfrench26@gmail.com>
2024-11-19 15:36:35 -0500

    Added socket connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  useEffect(() => {
+    const socket = new WebSocket("ws://localhost:8080");
+
+    socket.addEventListener("open", () => {
+      socket.send(
+        JSON.stringify({
+          message: 'Hello from React',
+        }),
+      );
+    });
+  }, []);
 
   return (
     <>
